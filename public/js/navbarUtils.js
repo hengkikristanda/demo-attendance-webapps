c1ec6530ce8e815dd6d497d6bbde1b2be80785b4
hengkikristanda <hengki.kristanda@binus.ac.id.com>
2024-06-21 20:45:11 +0700

    footer translation and chat widget

---
+++ public/js/navbarUtils.js
@@ -0,0 +1,50 @@
+function handleIcon_onClicked() {
+	const navbarMobile = document.getElementById("navbar-mobile");
+	navbarMobile.classList.toggle("show");
+}
+
+function toggleNavBarMobileMenu(event) {
+	const targetList = event.target.nextElementSibling;
+	if (targetList.classList.contains("showDropdown")) {
+		targetList.classList.remove("showDropdown");
+		targetList.classList.add("hideDropdown");
+	} else if (targetList.classList.contains("hideDropdown")) {
+		targetList.classList.remove("hideDropdown");
+		targetList.classList.add("showDropdown");
+	}
+}
+
+function outsideClickListener(event) {
+	const container = document.querySelector(".navbar-mobile__language ul");
+	const button = document.querySelector("#translation-icon__mobile");
+	if (!container.contains(event.target) && !button.contains(event.target)) {
+		container.style.display = "none";
+		document.removeEventListener("click", outsideClickListener);
+	}
+}
+
+function toggleMobileLanguage() {
+	const navbarMobile = document.getElementById("navbar-mobile");
+	navbarMobile.classList.remove("show");
+
+	const targetList = translationIconMobile.nextElementSibling;
+
+	if (targetList.style.display === "none" || targetList.style.display === "") {
+		targetList.style.display = "flex";
+		document.addEventListener("click", outsideClickListener);
+	} else {
+		targetList.style.display = "none";
+		document.removeEventListener("click", outsideClickListener);
+	}
+}
+
+const listNavbarMobile = document.getElementById("list-navbar__mobile");
+listNavbarMobile.addEventListener("mouseup", handleIcon_onClicked);
+
+const translationIconMobile = document.getElementById("translation-icon__mobile");
+translationIconMobile.addEventListener("mouseup", toggleMobileLanguage);
+
+const navbarMobileLabel = document.querySelectorAll(".navbar-mobile__label");
+for (let n = 0; n < navbarMobileLabel.length; n++) {
+	navbarMobileLabel[n].addEventListener("mouseup", toggleNavBarMobileMenu);
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -16,17 +16,29 @@ router.get("/", (req, res) => {
 		selectedLanguage = "id";
 	}
 
-	if (!["en", "id", "zh", "ja", "ko"].includes(selectedLanguage.toLowerCase())) selectedLanguage = "id";
+	if (!["en", "id", "zh", "ja", "ko"].includes(selectedLanguage.toLowerCase()))
+		selectedLanguage = "id";
 
 	const translations = JSON.parse(
-		fs.readFileSync(path.join(__dirname, "../translations/navbar/", `${selectedLanguage}.json`), "utf8")
+		fs.readFileSync(
+			path.join(__dirname, "../translations/navbar/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	const footerTranslation = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/footer/", `${selectedLanguage}.json`),
+			"utf8"
+		)
 	);
 
 	res.locals.translations = translations;
+	res.locals.footerTranslation = footerTranslation;
 
 	res.render("index", {
 		title: "Attendance System - Home",
-		selectedLanguage: "ID",
+		selectedLanguage,
 	});
 });
 
