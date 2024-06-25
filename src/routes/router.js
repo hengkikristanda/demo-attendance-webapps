73263faa8d91bfb5b1505c4d5ea7a82e8c062310
hengkikristanda <hengki.kristanda@binus.ac.id.com>
2024-06-25 21:44:31 +0700

    add page dosen

--- src/routes/router.js
+++ src/routes/router.js
@@ -33,16 +33,63 @@ router.get("/", (req, res) => {
 		)
 	);
 
+	const alumnies = JSON.parse(
+		fs.readFileSync(path.join(__dirname, "../translations/alumnies.json"), "utf8")
+	);
+
 	const lecturers = JSON.parse(
 		fs.readFileSync(path.join(__dirname, "../translations/lecturers.json"), "utf8")
 	);
 
 	res.locals.translations = translations;
 	res.locals.footerTranslation = footerTranslation;
+	res.locals.alumnies = alumnies;
 	res.locals.lecturers = lecturers;
 
 	res.render("index", {
 		title: "Attendance System - Home",
+		selectedLanguage,
+	});
+});
+
+router.get("/dosen", (req, res) => {
+	let selectedLanguage = req.query.lang;
+	if (selectedLanguage == undefined) {
+		selectedLanguage = "id";
+	}
+
+	if (!["en", "id", "zh", "ja", "ko"].includes(selectedLanguage.toLowerCase()))
+		selectedLanguage = "id";
+
+	const translations = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/navbar/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	const footerTranslation = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/footer/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	const alumnies = JSON.parse(
+		fs.readFileSync(path.join(__dirname, "../translations/alumnies.json"), "utf8")
+	);
+
+	const lecturers = JSON.parse(
+		fs.readFileSync(path.join(__dirname, "../translations/lecturers.json"), "utf8")
+	);
+
+	res.locals.translations = translations;
+	res.locals.footerTranslation = footerTranslation;
+	res.locals.alumnies = alumnies;
+	res.locals.lecturers = lecturers;
+
+	res.render("dosen", {
+		title: "Attendance System - Dosen",
 		selectedLanguage,
 	});
 });
