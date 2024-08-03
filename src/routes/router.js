4c7a091d8895b784aec2cac7665ad97b3c1ad6b9
hengkikristanda <hengki.kristanda@binus.ac.id.com>
2024-08-03 16:25:25 +0700

    add our purpose page

--- src/routes/router.js
+++ src/routes/router.js
@@ -450,6 +450,38 @@ router.get("/history", (req, res) => {
 	});
 });
 
+router.get("/our-purpose", (req, res) => {
+	let selectedLanguage = req.query.lang;
+	if (selectedLanguage == undefined) {
+		selectedLanguage = "id";
+	}
+
+	if (!["en", "id", "zh", "ja", "ko"].includes(selectedLanguage.toLowerCase()))
+		selectedLanguage = "id";
+
+	const translations = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/navbar/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	const footerTranslation = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/footer/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	res.locals.translations = translations;
+	res.locals.footerTranslation = footerTranslation;
+
+	res.render("ourPurpose/index", {
+		title: "PTDI STTD - Vision & Mission",
+		selectedLanguage,
+	});
+});
+
 function searchById(data, id) {
 	console.log("Search JSON by id: ", id);
 
