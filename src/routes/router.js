1a8b94574d4f84670bcd89ec320516380136e25c
hengkikristanda <hengki.kristanda@binus.ac.id.com>
2024-08-05 22:50:34 +0700

    add tugas dan fungsi page

--- src/routes/router.js
+++ src/routes/router.js
@@ -482,6 +482,70 @@ router.get("/our-purpose", (req, res) => {
 	});
 });
 
+router.get("/our-purpose", (req, res) => {
+	let selectedLanguage = req.query.lang;
+	if (selectedLanguage == undefined) {
+		selectedLanguage = "id";
+	}
+
+	if (!["en", "id", "zh", "ja", "ko"].includes(selectedLanguage.toLowerCase()))
+		selectedLanguage = "id";
+
+	const translations = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/navbar/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	const footerTranslation = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/footer/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	res.locals.translations = translations;
+	res.locals.footerTranslation = footerTranslation;
+
+	res.render("ourPurpose/index", {
+		title: "PTDI STTD - Vision & Mission",
+		selectedLanguage,
+	});
+});
+
+router.get("/duties", (req, res) => {
+	let selectedLanguage = req.query.lang;
+	if (selectedLanguage == undefined) {
+		selectedLanguage = "id";
+	}
+
+	if (!["en", "id", "zh", "ja", "ko"].includes(selectedLanguage.toLowerCase()))
+		selectedLanguage = "id";
+
+	const translations = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/navbar/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	const footerTranslation = JSON.parse(
+		fs.readFileSync(
+			path.join(__dirname, "../translations/footer/", `${selectedLanguage}.json`),
+			"utf8"
+		)
+	);
+
+	res.locals.translations = translations;
+	res.locals.footerTranslation = footerTranslation;
+
+	res.render("duties/index", {
+		title: "PTDI STTD - Tugas & Fungsi",
+		selectedLanguage,
+	});
+});
+
 function searchById(data, id) {
 	console.log("Search JSON by id: ", id);
 
