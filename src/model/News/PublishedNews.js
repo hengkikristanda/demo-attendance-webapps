747e515c8278a5d15ec587ee92ebdc59573dea26
hengkikristanda <hengki.kristanda@binus.ac.id.com>
2024-11-28 12:57:19 +0700

    published news

--- src/model/News/PublishedNews.js
+++ src/model/News/PublishedNews.js
@@ -2,38 +2,39 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../../config/database");
 
 const PublishedNews = sequelize.define(
-	"PublishedNews",
-	{
-		id: {
-			type: DataTypes.STRING(36),
-			primaryKey: true,
-			allowNull: false,
-			unique: true,
-		},
-		title: {
-			type: DataTypes.STRING(50),
-			allowNull: false,
-		},
-		content: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-		},
-		display_image_id: {
-			type: DataTypes.STRING(36),
-			allowNull: false,
-		},
-		mime_type: {
-			type: DataTypes.STRING(16),
-			allowNull: true,
-		},
-		last_modified: {
-			type: DataTypes.DATE,
-			allowNull: true,
-		},
-	},
-	{
-		tableName: "published_news",
-		timestamps: false, // Disable created_at and updated_at
-	}
+    "PublishedNews",
+    {
+        id: {
+            type: DataTypes.STRING(36),
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        title: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        display_image_id: {
+            type: DataTypes.STRING(36),
+            allowNull: false,
+        },
+        mime_type: {
+            type: DataTypes.STRING(16),
+            allowNull: true,
+        },
+        last_modified: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: "published_news",
+        timestamps: false, // Disable created_at and updated_at
+    }
 );
+
 module.exports = PublishedNews;
